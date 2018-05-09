a0eae96d1b76623bdf503c312261cfc653b0db31
unnatjain <unnatjain@gmail.com>
2018-05-09 03:12:51 -0500

    combined active learning from scratch

--- test_dataset.py
+++ test_dataset.py
@@ -1,5 +1,8 @@
 from gcn_ner import GCNNer
+import sys
 
 if __name__ == '__main__':
-    ner = GCNNer(ner_filename='./data/unlabeled_50/ner-gcn-9.tf', trans_prob_file='./data/trans_prob.pickle')
-    ner.test('./data/labeled.conll')
+    # ner = GCNNer(ner_filename='./data/unlabeled_50/ner-gcn-9.tf', trans_prob_file='./data/trans_prob.pickle')
+    ner = GCNNer(ner_filename='./data/unlabeled_50_uncertain/ner-gcn-{}.tf'.format(sys.argv[1]), trans_prob_file='./data/trans_prob.pickle')
+    print('./data/unlabeled_50_uncertain/ner-gcn-{}.tf'.format(sys.argv[1]))
+    ner.test('./data/dev.conll')
--- train.py
+++ train.py
@@ -2,13 +2,42 @@ from gcn_ner import GCNNer
 
 if __name__ == '__main__':
 	
-	# from numpy import genfromtxt
-	# import numpy as np
+	from numpy import genfromtxt
+	import numpy as np
+	
+	# al_length = 3750
+	# al_list = list(np.random.randint(0,45112,al_length))
+	# GCNNer.train_and_save(dataset='./data/labeled.conll', saving_dir='./data/unlabeled_50_random', epochs=20, al_args=al_list, load_ckpt="./data/unlabeled_50/ner-gcn-9.tf")
+
 	# my_data = genfromtxt('unlabeled_50_scores_sorted.csv', delimiter=',')
 	# al_length = 3750
 	# al_list = list(my_data[:3750,0].astype(np.int))
+	# print("Total finetuning samples: {}".format(len(al_list)))
+	# GCNNer.train_and_save(dataset='./data/labeled.conll', saving_dir='./data/unlabeled_50_uncertain_2', epochs=20, al_args=al_list, load_ckpt="./data/unlabeled_50/ner-gcn-9.tf")
 
-	# GCNNer.train_and_save(dataset='./data/labeled.conll', saving_dir='./data/unlabeled_50_uncertain', epochs=10, al_args=al_list, load_ckpt="./data/unlabeled_50/ner-gcn-9.tf")
 
-	al_fraction = 0.08312644085
-	GCNNer.train_and_save(dataset='./data/labeled.conll', saving_dir='./data/unlabeled_50_random', epochs=10, al_args=al_fraction, load_ckpt="./data/unlabeled_50/ner-gcn-9.tf")
+	# my_data = genfromtxt('unlabeled_50_scores_sorted.csv', delimiter=',')
+	# al_length = 3750
+	# al_list = list(my_data[:3750,0].astype(np.int))
+	# al_list.extend(range(45112, 45112+15177))
+	# print("Total finetuning samples: {}".format(len(al_list)))
+	# GCNNer.train_and_save(dataset='./data/labeled_and_unlabeled_50.conll', saving_dir='./data/unlabeled_50_uncertain_combined', epochs=20, al_args=al_list, load_ckpt="./data/unlabeled_50/ner-gcn-9.tf")
+
+	# al_length = 3750
+	# al_list = list(np.random.randint(0,45112,al_length))
+	# al_list.extend(range(45112, 45112+15177))
+	# print("Total finetuning samples: {}".format(len(al_list)))
+	# GCNNer.train_and_save(dataset='./data/labeled_and_unlabeled_50.conll', saving_dir='./data/unlabeled_50_random_combined', epochs=20, al_args=al_list, load_ckpt="./data/unlabeled_50/ner-gcn-9.tf")
+
+	my_data = genfromtxt('unlabeled_50_scores_sorted.csv', delimiter=',')
+	al_length = 3750
+	al_list = list(my_data[:3750,0].astype(np.int))
+	al_list.extend(range(45112, 45112+15177))
+	print("Total finetuning samples: [UC] {}".format(len(al_list)))
+	GCNNer.train_and_save(dataset='./data/labeled_and_unlabeled_50.conll', saving_dir='./data/unlabeled_50_uncertain_combined_scratch', epochs=30, al_args=al_list)
+
+	al_length = 3750
+	al_list = list(np.random.randint(0,45112,al_length))
+	al_list.extend(range(45112, 45112+15177))
+	print("Total finetuning samples: [Random] {}".format(len(al_list)))
+	GCNNer.train_and_save(dataset='./data/labeled_and_unlabeled_50.conll', saving_dir='./data/unlabeled_50_random_combined_scratch', epochs=30, al_args=al_list)
